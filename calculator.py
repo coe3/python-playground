ad6ee2b27bb1c25c8f1d6299d70e666fbd25c012
CHIBUZO <CHE429@MAIL.HARVARD.EDU>
2017-01-11 17:54:54 -0500

    add colon to the end of line 10 to complete function

--- calculator.py
+++ calculator.py
@@ -7,9 +7,10 @@ def divide(a,b):
 def add(a,b):
 	return a + b
 
-def subtract(a,b)
+def subtract(a,b):
 	return a - b 
 
 print "I'm going use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
 print x
+
