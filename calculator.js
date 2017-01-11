53b755be7f4ccaa7f6a4c7814e983793a97a39ba
CHIBUZO <CHE429@MAIL.HARVARD.EDU>
2017-01-11 18:20:49 -0500

    add comments correctly in JS

--- calculator.js
+++ calculator.js
@@ -1,17 +1,21 @@
-# Create a calculator using javascript
+// Add a calculator
 
+//Multiply
 function multiply(a,b) { 
     return (a * b)
 }
 
+//Divide
 function divide(a,b) {
 	return (a / b)
 }
 
+//Add
 function add(a,b) {
 	return (a + b)
 }
 
+//Subtract
 function subtract(a,b) {
 	return (a - b )
 }
