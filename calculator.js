8aff1969f1bf68b9f1be273d3db2940dce0b984d
CHIBUZO <CHE429@MAIL.HARVARD.EDU>
2017-01-11 18:16:21 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+# Create a calculator using javascript
+
+function multiply(a,b) { 
+    return (a * b)
+}
+
+function divide(a,b) {
+	return (a / b)
+}
+
+function add(a,b) {
+	return (a + b)
+}
+
+function subtract(a,b) {
+	return (a - b )
+}
